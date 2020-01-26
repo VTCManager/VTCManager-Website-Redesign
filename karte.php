75eb86789e4615b3b61b2be969a3778541acb412
EPCookie <59062265+EPCookie@users.noreply.github.com>
2020-01-26 02:29:02 +0100

    Removed Search, Editet Header

--- karte.php
+++ karte.php
@@ -140,19 +140,10 @@ position:absolute;
         <div class="card-body d-sm-flex justify-content-between">
 
           <h4 class="mb-2 mb-sm-0 pt-1">
-            <a href="https://mdbootstrap.com/docs/jquery/" target="_blank">Home Page</a>
+            <a href="dashboard">Homepage</a>
             <span>/</span>
-            <span>Dashboard</span>
+            <span>Karte</span>
           </h4>
-
-          <form class="d-flex justify-content-center">
-            <!-- Default input -->
-            <input type="search" placeholder="Type your query" aria-label="Search" class="form-control">
-            <button class="btn btn-primary btn-sm my-0 p" type="submit">
-              <i class="fas fa-search"></i>
-            </button>
-
-          </form>
 
         </div>
 
