e50dd746a85011380741801cacbc14649b8fea59
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-02 10:50:06 +0100

    added yellow badge

--- clientarea/php/truckyapi.php
+++ clientarea/php/truckyapi.php
@@ -5,7 +5,9 @@ for($i=0; $i < 10; $i++) {
   echo '<a class="list-group-item list-group-item-action waves-effect">'.$obj_trucky->response[$i]->name;
   if($obj_trucky->response[$i]->severity == 'Congested' or $obj_trucky->response[$i]->severity == 'Heavy') {
     echo ' <span class="badge badge-danger badge-pill pull-right">'.$obj_trucky->response[$i]->players.'</span>';
-  } else {
+  } else if ($obj_trucky->response[$i]->severity == 'Moderate') {
+    echo ' <span class="badge badge-warning badge-pill pull-right">'.$obj_trucky->response[$i]->players.'</span>';
+  }else {
     echo ' <span class="badge badge-primary badge-pill pull-right">'.$obj_trucky->response[$i]->players.'</span>';
   }
   echo '</a>';
