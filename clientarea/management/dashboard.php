2781b32ef5f3caeec2d7ced011fee5cc23b22706
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-02 12:27:34 +0100

    added loading spinner

--- clientarea/management/dashboard.php
+++ clientarea/management/dashboard.php
@@ -19,6 +19,11 @@ $page_now = "management/dashboard";
   <link href="/clientarea/management/css/mdb.min.css" rel="stylesheet">
   <!-- Your custom styles (optional) -->
   <link href="/clientarea/management/css/style.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script>$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});</script>
   <style>
 
     .map-container{
@@ -35,9 +40,22 @@ width:100%;
 position:absolute;
 }
   </style>
+  <style>.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(img/loader.gif) center no-repeat #fff;
+}
+</style>
 </head>
 
 <body class="grey lighten-3">
+  <div class="se-pre-con"></div>
 
   <!--Main Navigation-->
   <header>
