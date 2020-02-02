e211f3ee58506bd3fa627d0c6d36d133b8d22e72
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-02 16:00:12 +0100

    changed links and compressed mp4

--- home/index.php
+++ home/index.php
@@ -9,11 +9,11 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="/home/css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
-  <link href="css/mdb.min.css" rel="stylesheet">
+  <link href="/home/css/mdb.min.css" rel="stylesheet">
   <!-- Your custom styles (optional) -->
-  <link href="css/style.min.css" rel="stylesheet">
+  <link href="/home/css/style.min.css" rel="stylesheet">
   <style type="text/css">
     html,
     body,
@@ -158,7 +158,7 @@
 
           <!--Video source-->
           <video class="video-intro" autoplay loop muted>
-            <source src="vid/vid1.mp4" type="video/mp4">
+            <source src="vid/vid2.mp4" type="video/mp4">
           </video>
 
           <!-- Mask & flexbox options-->
@@ -764,13 +764,13 @@
 
   <!-- SCRIPTS -->
   <!-- JQuery -->
-  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="/home/js/jquery-3.4.1.min.js"></script>
   <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="/home/js/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="/home/js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="/home/js/mdb.min.js"></script>
   <!-- Initializations -->
   <script type="text/javascript">
     // Animations initialization
