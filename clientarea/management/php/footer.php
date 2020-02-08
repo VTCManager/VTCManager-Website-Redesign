4659b8ff2a3026cf563ec5ccd299fecf4c8e9eb0
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-08 20:54:32 +0100

    added footer.php

---
+++ clientarea/management/php/footer.php
@@ -0,0 +1,41 @@
+<footer class="page-footer text-center font-small primary-color-dark darken-2 mt-4 wow fadeIn">
+
+    <!--Call to action-->
+    <div class="pt-4">
+      <a class="btn btn-outline-white" href="/download" target="_blank"
+        role="button">Download
+        <i class="fas fa-download ml-2"></i>
+      </a>
+    </div>
+    <!--/.Call to action-->
+
+    <hr class="my-4">
+
+    <!-- Social icons -->
+    <div class="pb-4">
+      <a href="https://www.instagram.com/tnwm_group/" target="_blank">
+        <i class="fab fa-instagram mr-3"></i>
+      </a>
+
+      <a href="https://twitter.com/TNWM_group" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+      </a>
+
+      <a href="https://www.youtube.com/" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+
+      <a href="https://github.com/TheNorthWestMediaGroup" target="_blank">
+        <i class="fab fa-github mr-3"></i>
+      </a>
+    </div>
+    <!-- Social icons -->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2020 Copyright:
+      <a href="https://northwestvideo.de" target="_blank"> TheNorthWestMedia Group </a>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
