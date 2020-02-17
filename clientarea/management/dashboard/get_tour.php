4cfb10f317a4760dcec0dc73da50797a44891ee3
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-17 13:17:12 +0100

    added comments and exception

--- clientarea/management/dashboard/get_tour.php
+++ clientarea/management/dashboard/get_tour.php
@@ -1,8 +1,17 @@
 <?php
+//Zweck: hole Daten für detaillierte Ansicht des Auftrages
+//check GET request
+if(!isset($_GET['username']) && !isset($_GET['tour_id'])){
+    //bad request
+    header("Status: 400 Bad Request");
+    die();
+    }
+//Connect and Check
 include '../../../basis_files/php/get_user_data.php';
+//GET Variablen
 $requested_tour_id= $_GET['tour_id'];
 $requested_username= $_GET['username'];
-//hole alle Touren aus der DB
+//lade Tour Daten aus DB
 $sql = "SELECT * FROM tour_table WHERE tour_id=$requested_tour_id AND username='$requested_username'";
 $result = $conn->query($sql);
 
@@ -12,4 +21,8 @@ if ($result->num_rows > 0) {
 		$rows[] = $row;
 	}
 	echo json_encode($rows);
-}
+}else{
+	}
+//close DB conn
+$conn->close();
+?>
