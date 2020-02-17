434590cdd092929a21b0b588256c31beda369f6d
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-17 13:26:37 +0100

    added comments

--- clientarea/management/php/get_earned_money.php
+++ clientarea/management/php/get_earned_money.php
@@ -1,73 +1,88 @@
-
 <?php
+//connect and Check
 include '../../../basis_files/php/get_user_data.php'; 
+//Abfrage aktuelle Woche
 $day = date('w');
 $week_start = date('Y-m-d', strtotime('-'.$day.' days + 1 day'));
 $date = new DateTime();
 $week = $date->format("W");
+//hole Umsätze aus dieser Woche
 $sql = "SELECT amount FROM money_transfer WHERE receiver='$user_company_name' AND DATE( date_sent ) >= '$week_start'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
+        //zähle Geld
         $current_amount = $current_amount + (int)$row["amount"];
     }
+    //wenn Ergebnisse gefunden
     $array = [
     "week" => $week,
     "amount" => $current_amount,
     ];
-    }else{
-        $array = [
+}else{
+    //wenn keine Ergebnisse gefunden
+    $array = [
     "week" => $week,
     "amount" => 0,
     ];
-        }
-        $current_amount = 0;
-    $day = date('w');
+ }
+ //Geld zurücksetzen
+$current_amount = 0;
+//Abfrage letzte Woche
+$day = date('w');
 $week_start2 = date('Y-m-d',strtotime("$week_start -7 days",));
 $week = date('W', strtotime('-7 days'));
+//hole Umsätze
 $sql = "SELECT amount FROM money_transfer WHERE receiver='$user_company_name' AND DATE( date_sent ) >= '$week_start2'AND DATE( date_sent ) <= '$week_start'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
+        //zähle Geld
         $current_amount = $current_amount + (int)$row["amount"];
     }
+    //wenn Ergebnisse gefunden
     $array2 = [
     "week" => $week,
     "amount" => $current_amount,
     ];
 }else{
+    //wenn keine Ergebnisse gefunden
         $array2 = [
     "week" => $week,
     "amount" => 0,
     ];
         }
-        $current_amount = 0;
-        $day = date('w');
+//Geld zurücksetzen
+$current_amount = 0;
+//Abfrage vorletzte Woche
+$day = date('w');
 $week_start3 = date('Y-m-d',strtotime("$week_start2 -7 days",));
 $week = date('W', strtotime('-14 days'));
+//hole Umsätze
 $sql = "SELECT amount FROM money_transfer WHERE receiver='$user_company_name' AND DATE( date_sent ) >= '$week_start3'AND DATE( date_sent ) <= '$week_start2'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
+        //zähle Geld
         $current_amount = $current_amount + (int)$row["amount"];
     }
+    //wenn Ergebnisse gefunden
     $array3 = [
     "week" => $week,
     "amount" => $current_amount,
     ];
 }else{
+    //wenn keine Ergebnisse gefunden
         $array3 = [
     "week" => $week,
     "amount" => 0,
     ];
         }
                 $current_amount = 0;
+                //Abfrage vorvorletzte Woche
         $day = date('w');
 $week_start4 = date('Y-m-d',strtotime("$week_start3 -7 days",));
 $week = date('W', strtotime('-21 days'));
@@ -90,6 +105,7 @@ if ($result->num_rows > 0) {
     ];
         }
                 $current_amount = 0;
+                //Abfrage vorvorvorletzte Woche
         $day = date('w');
 $week_start3 = date('Y-m-d',strtotime("$week_start4 -7 days",));
 $week = date('W', strtotime('-28 days'));
