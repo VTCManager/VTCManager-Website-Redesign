3b911b26d8727763d79082046a59e1d2b1b2b1e4
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-23 10:13:14 +0100

    change career entry

--- clientarea/management/dashboard/employees/fire_employee.php
+++ clientarea/management/dashboard/employees/fire_employee.php
@@ -20,6 +20,7 @@ $employeeID = $_POST['employeeID'];
 
 //
 $sql = "UPDATE career_table SET fire_reason='$reason_text', end_date=NOW() WHERE userID=$employeeID AND end_date=0000-00-00";
+echo $sql;
 
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
