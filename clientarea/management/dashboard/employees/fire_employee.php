fc9754133ac6a412d35e8722c49a4640ac261775
Joschua Haß <joschua.hass.sh@gmail.com>
2020-02-23 10:59:55 +0100

    added email system

--- clientarea/management/dashboard/employees/fire_employee.php
+++ clientarea/management/dashboard/employees/fire_employee.php
@@ -18,15 +18,27 @@ if($EditEmployees != "1"){
 $reason_text = $_POST['reason'];
 $employeeID = $_POST['employeeID'];
 
-//
+//Eintrag im Lebenslauf anpassen und Grund setzen
 $sql = "UPDATE career_table SET fire_reason='$reason_text', end_date=NOW() WHERE userID=$employeeID AND end_date=0000-00-00";
-echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
 } else {
     echo "Error updating record: " . $conn->error;
+    die();
 }
+include 'emails/fire_employee/fire_employee.php';
+$to = 'joschua.hass.sh@gmail.com';
+
+$subject = 'Du wurdest aus deiner Spedition entlassen|VTCManager';
+
+$headers = "From: service@northwestvideo.de\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+$message = $fire_employee_email_content;
+
+
+mail($to, $subject, $message, $headers);
 //close DB conn
 $conn->close();
 ?>
