4d5fb3e3b74c4553711bcb3a873bd3aa3ab6cd7a
EPCookie <59062265+EPCookie@users.noreply.github.com>
2020-03-03 20:37:21 +0100

    mobile color

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <link rel="icon" href="/media/images/favicon.png" type="image/x-icon">
+  <meta name="theme-color" content="rgba(0, 255, 229, 1)">
   <title>VTCManager</title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
