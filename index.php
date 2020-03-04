ff484ee13ee7f9bc7b1b65136b3ce91f211a868c
Joschua Haß <joschua.hass.sh@gmail.com>
2020-03-04 15:36:34 +0100

    navbar fix

--- index.php
+++ index.php
@@ -10,62 +10,7 @@
 <body>
 
   <!-- Navbar -->
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
-    <div class="container">
-
-      <!-- Brand -->
-      <a class="navbar-brand">
-        <img src="/media/images/favicon.png" height="30">
-      </a>
-
-      <!-- Collapse -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <!-- Links -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <!-- Left -->
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="team" target="_blank">Team</a>
-          </li>
-        </ul>
-
-        <!-- Right -->
-        <ul class="navbar-nav nav-flex-icons">
-          <li class="nav-item">
-            <a href="https://www.instagram.com/tnwm_group/" class="nav-link" target="_blank">
-              <i class="fab fa-instagram"></i>
-              </a>
-          </li>
-          <li class="nav-item">
-            <a href="https://twitter.com/TNWM_group" class="nav-link" target="_blank">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/account/login" class="nav-link border border-light rounded">
-              <?php if(!isset($_COOKIE['authWebToken']) && !isset($_COOKIE['username'])) {?>
-              <i class="fas fa-sign-in-alt mr-2"></i>Anmelden
-              <?php }else{ ?>
-                <i class="fas fa-sign-in-alt mr-2"></i>Öffnen
-                <?php } ?>
-            </a>
-          </li>
-        </ul>
-
-      </div>
-
-    </div>
-  </nav>
+  <?php include 'home/navbar.php'; ?>
   <!-- Navbar -->
 
   <!--Carousel Wrapper-->
