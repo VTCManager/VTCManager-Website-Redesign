64646dab9cf245637f845e05092c33cf3df64a18
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-03-08 16:56:07 +0100

    map update

--- clientarea/management/karte.php
+++ clientarea/management/karte.php
@@ -19,20 +19,20 @@ $page_now = "management/map";
   <!-- Your custom styles (optional) -->
   <link href="/clientarea/management/css/style.min.css" rel="stylesheet">
   <style>
+    .map-container {
+      overflow: hidden;
+      padding-bottom: 56.25%;
+      position: relative;
+      height: 0;
+    }
 
-    .map-container{
-overflow:hidden;
-padding-bottom:56.25%;
-position:relative;
-height:0;
-}
-.map-container iframe{
-left:0;
-top:0;
-height:100%;
-width:100%;
-position:absolute;
-}
+    .map-container iframe {
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      position: absolute;
+    }
   </style>
 </head>
 
@@ -42,8 +42,8 @@ position:absolute;
   <header>
 
     <!-- Sidebar -->
-    <?php 
-    include 'php/sidebar.php';?>
+    <?php
+    include 'php/sidebar.php'; ?>
     <!-- Sidebar -->
 
   </header>
@@ -60,7 +60,7 @@ position:absolute;
         <div class="card-body d-sm-flex elegant-color white-text justify-content-between">
 
           <h4 class="mb-2 mb-sm-0 pt-1">
-            <a href="dashboard">Homepage</a>
+            <a href="/clientarea/management/">Spedition</a>
             <span>/</span>
             <span>Firmekarte</span>
           </h4>
@@ -71,20 +71,19 @@ position:absolute;
       <!-- Heading -->
 
       <!--Grid row-->
-      <div class="row wow fadeIn" >
+      <div class="row wow fadeIn">
 
         <!--Grid column-->
         <div class="col-md-9 mb-4">
 
           <!--Card-->
-          <div class="card" >
+          <div class="card">
 
             <!--Card content-->
             <div class="card-body elegant-color white-text">
-				<div id="map-container" class="z-depth-1-half map-container" style="height: 500px">
-  <iframe src="/map/" frameborder="0"
-    style="border:0" allowfullscreen></iframe>
-</div>
+              <div id="map-container" class="z-depth-1-half map-container" style="height: 500px">
+                <iframe src="/map/" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
 
             </div>
 
@@ -101,15 +100,16 @@ position:absolute;
           <div class="card mb-4">
 
             <!--Card content-->
-            <div class="card-body elegant-color white-text" >
+            <div class="card-body elegant-color white-text">
 
               <div class="card-header text-center unique-color white-text">
-                <?php include 'php/tmp-api.php';//load Server Stats?>
-                Verkehr Sim 1 [<?php echo $sim1_players;?>]
+                <?php include 'php/tmp-api.php'; //load Server Stats
+                ?>
+                Verkehr Sim 1 [<?php echo $sim1_players; ?>]
               </div>
               <!-- List group links -->
               <div class="list-group list-group-flush">
-                <?php include 'php/truckyapi.php';?>
+                <?php include 'php/truckyapi.php'; ?>
               </div>
               <!-- List group links -->
 
@@ -129,68 +129,7 @@ position:absolute;
   <!--Main layout-->
 
   <!--Footer-->
-  <footer class="page-footer text-center font-small primary-color-dark darken-2 mt-4 wow fadeIn">
-
-    <!--Call to action-->
-    <div class="pt-4">
-      <a class="btn btn-outline-white" href="https://mdbootstrap.com/docs/jquery/getting-started/download/" target="_blank"
-        role="button">Download
-        MDB
-        <i class="fas fa-download ml-2"></i>
-      </a>
-      <a class="btn btn-outline-white" href="https://mdbootstrap.com/education/bootstrap/" target="_blank" role="button">Start
-        free tutorial
-        <i class="fas fa-graduation-cap ml-2"></i>
-      </a>
-    </div>
-    <!--/.Call to action-->
-
-    <hr class="my-4">
-
-    <!-- Social icons -->
-    <div class="pb-4">
-      <a href="https://www.facebook.com/mdbootstrap" target="_blank">
-        <i class="fab fa-facebook-f mr-3"></i>
-      </a>
-
-      <a href="https://twitter.com/MDBootstrap" target="_blank">
-        <i class="fab fa-twitter mr-3"></i>
-      </a>
-
-      <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
-        <i class="fab fa-youtube mr-3"></i>
-      </a>
-
-      <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
-        <i class="fab fa-google-plus mr-3"></i>
-      </a>
-
-      <a href="https://dribbble.com/mdbootstrap" target="_blank">
-        <i class="fab fa-dribbble mr-3"></i>
-      </a>
-
-      <a href="https://pinterest.com/mdbootstrap" target="_blank">
-        <i class="fab fa-pinterest mr-3"></i>
-      </a>
-
-      <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
-        <i class="fab fa-github mr-3"></i>
-      </a>
-
-      <a href="http://codepen.io/mdbootstrap/" target="_blank">
-        <i class="fab fa-codepen mr-3"></i>
-      </a>
-    </div>
-    <!-- Social icons -->
-
-    <!--Copyright-->
-    <div class="footer-copyright py-3">
-      © 2020 Copyright:
-      <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> NorthWestMedia </a>
-    </div>
-    <!--/.Copyright-->
-
-  </footer>
+  <?php include '../php/footer.php'; ?>
   <!--/.Footer-->
 
   <!-- SCRIPTS -->
@@ -206,127 +145,6 @@ position:absolute;
   <script type="text/javascript">
     // Animations initialization
     new WOW().init();
-
-  </script>
-
-  <!-- Charts -->
-  <script>
-    // Line
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-
-
-    //line
-    var ctxL = document.getElementById("lineChart").getContext('2d');
-    var myLineChart = new Chart(ctxL, {
-      type: 'line',
-      data: {
-        labels: ["Januar", "Februar", "März", "April", "Mai", "Juni"],
-        datasets: [{
-            label: "Gefahrene Touren",
-            backgroundColor: [
-              'rgba(105, 0, 132, .2)',
-            ],
-            borderColor: [
-              'rgba(200, 99, 132, .7)',
-            ],
-            borderWidth: 2,
-            data: [0, 0, 0, 0, 0, 0]
-          }
-        ]
-      },
-      options: {
-        responsive: true
-      }
-    });
-
-
-    //doughnut
-    var ctxD = document.getElementById("doughnutChart").getContext('2d');
-    var myLineChart = new Chart(ctxD, {
-      type: 'doughnut',
-      data: {
-        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-        datasets: [{
-          data: [300, 50, 100, 40, 120],
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-        }]
-      },
-      options: {
-        responsive: true
-      }
-    });
-
-  </script>
-
-  <script>
-    new Chart(document.getElementById("horizontalBar"), {
-      "type": "horizontalBar",
-      "data": {
-        "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
-        "datasets": [{
-          "label": "My First Dataset",
-          "data": [22, 33, 55, 12, 86, 23, 14],
-          "fill": false,
-          "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"
-          ],
-          "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)",
-            "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)",
-            "rgb(201, 203, 207)"
-          ],
-          "borderWidth": 1
-        }]
-      },
-      "options": {
-        "scales": {
-          "xAxes": [{
-            "ticks": {
-              "beginAtZero": true
-            }
-          }]
-        }
-      }
-    });
-
   </script>
 </body>
 
