ea1174a3dba81be0abf73ece5808c27419b5125e
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-03-08 15:18:20 +0100

    format change

--- clientarea/management/settings/create_rank.php
+++ clientarea/management/settings/create_rank.php
@@ -1,10 +1,10 @@
 <?php
 foreach ($_POST as $key => $value) {
     switch ($key) {
-		case 'salary':
+        case 'salary':
             $salary = $value;
             break;
-		case 'name':
+        case 'name':
             $requested_rank = $value;
             break;
         default:
@@ -13,15 +13,15 @@ foreach ($_POST as $key => $value) {
 }
 include '../get_user_data.php';
 //hat User berechtigung zum erstellen neuer Ränge?
-if($EditEmployees != "1"){
-	die("no permission");
+if ($EditEmployees != "1") {
+    die("no permission");
 }
-(int)$salary;
+(int) $salary;
 $sql = "INSERT INTO rank (name, forCompanyID, EditProfile, SeeLogbook, EditLogbook, SeeBank, UseBank, EditEmployees, EditSalary, salary, struct_id) VALUES ('$requested_rank',$user_company_id,0,0,0,0,0,0,0,$salary,0)";
 
 if ($conn->query($sql) === TRUE) {
 } else {
-    die( "Error: " . $sql . "<br>" . $conn->error);
+    die("Error: " . $sql . "<br>" . $conn->error);
 }
 header("Location: /clientarea/management/settings/");
-	exit();
+exit();
