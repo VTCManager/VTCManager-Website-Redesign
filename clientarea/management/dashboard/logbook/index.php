dccafdcd5209ee35c56997f757092c8459ad0112
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-03-08 17:24:09 +0100

    added head.php

--- clientarea/management/dashboard/logbook/index.php
+++ clientarea/management/dashboard/logbook/index.php
@@ -81,24 +81,10 @@ if (isset($_POST['jobID']) && isset($_POST['driverUserName']) && isset($_POST['c
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <link rel="icon" href="/clientarea/management/img/favicon.png" type="image/x-icon">
-  <link rel="apple-touch-icon" href="/clientarea/management/img/apple-icon.png">
-  <title>VTCMInterface</title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <!-- Bootstrap core CSS -->
-  <link href="/clientarea/management/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="/clientarea/management/css/mdb.min.css" rel="stylesheet">
-  <!-- Your custom styles (optional) -->
-  <link href="/clientarea/management/css/style.min.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <?php include '../../../head.php'; ?>
   <script>
     function delete_entry(elmnt) {
       var save_val = $(elmnt).attr("data-id");
@@ -123,6 +109,7 @@ if (isset($_POST['jobID']) && isset($_POST['driverUserName']) && isset($_POST['c
       color: white;
     }
   </style>
+</head>
 
 <body class="elegant-color-dark">
 
@@ -146,7 +133,6 @@ if (isset($_POST['jobID']) && isset($_POST['driverUserName']) && isset($_POST['c
   <?php //Lade Tour Prüfungs Fenster nur wenn User Berechtigung zum Bearbeiten des Logbuches hat
   if ($EditLogbook == "1") {
     include 'modals/tour_check.php'; ?>
-    </head>
   <?php }
   include 'modals/tour_view.php'; ?>
   <main class="pt-5 mx-lg-5">
